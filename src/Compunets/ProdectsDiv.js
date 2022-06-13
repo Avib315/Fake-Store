db898631f19c2dc200a0d0aed87eb8c9b4532eb1
avi barashi <avibarashi315248@gamil.com>
2022-06-13 15:25:34 +0300

    move btn in prodectDiv

--- src/Compunets/ProdectsDiv.js
+++ src/Compunets/ProdectsDiv.js
@@ -4,15 +4,14 @@ import BuyInCart from "./BuyInCart"
 import Style from './ProdectsDiv.module.css'
 export default function ProdectsDiv(props){
     const inCart = props.inCart ? props.inCart.inCart : 0
-    console.log(inCart);
     return (
         <>
                 <Link to={'/item/' + props.id}>
                 <div className={Style.ProdectsDiv}>
                     <img src={props.img}  className = {Style.pic}/>
+                 {inCart > 0 ?  <BuyInCart inCart={inCart} item={props.item} addToCart={props.addToCart} removeFromCart={props.removeFromCart}/> : <BuyButton item={props.item} addToCart={props.addToCart}/>}  
                     <div>
                         <h3>{props.title}  </h3>
-                 {inCart > 0 ?  <BuyInCart inCart={inCart} item={props.item} addToCart={props.addToCart} removeFromCart={props.removeFromCart}/> : <BuyButton item={props.item} addToCart={props.addToCart}/>}  
                     <h4>price :<span className={Style.priceSpan}>  {props.price}$ </span> </h4>    
                     </div>
                 </div>
